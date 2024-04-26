421550aee949f26b01c750a82f0e0bf11118e9e4
Emma A <emmaalderson24@gmail.com>
2024-04-26 17:59:47 -0500

    reorganised some of the main function

--- src/Smot.cpp
+++ src/Smot.cpp
@@ -28,6 +28,11 @@ struct ProgramArgs {
   std::string path_to_screenshots;
 };
 
+struct XEnvironment {
+  Display *display;
+  Window root_window;
+};
+
 int32_t load_args(int argc, char **argv, ProgramArgs *args) {
   bool scrot_folder_provided = false;
   args->verbose_mode = false;
@@ -51,27 +56,59 @@ int32_t load_args(int argc, char **argv, ProgramArgs *args) {
   return 0;
 }
 
-int main(int argc, char **argv) {
-  ProgramArgs args;
-  int32_t valid_args = load_args(argc, argv, &args);
-  if (valid_args != 0)
-    return 1;
+std::string build_scrot_command(ProgramArgs args,
+                                ScrotRectangle scrot_rectangle) {
+  std::string file_name_template = "%Y-%m-%d-%T-screenshot.png";
+  std::string options =
+      "-a " + std::to_string(scrot_rectangle.x1) + "," +
+      std::to_string(scrot_rectangle.y1) + "," +
+      std::to_string(scrot_rectangle.x2 - scrot_rectangle.x1) + "," +
+      std::to_string(scrot_rectangle.y2 - scrot_rectangle.y1) + " ";
 
-  Display *display = XOpenDisplay("");
-  if (display == NULL) {
+  std::string command =
+      "scrot " + options + " " + args.path_to_screenshots + file_name_template;
+
+  if (args.verbose_mode) {
+    std::cout << "Scrot Rectangle: " << std::endl;
+    std::cout << "[ " << scrot_rectangle.x1 << ", " << scrot_rectangle.y1
+              << " ]" << std::endl;
+    std::cout << "[ " << scrot_rectangle.x2 - scrot_rectangle.x1 << ", "
+              << scrot_rectangle.y2 - scrot_rectangle.y1 << " ]" << std::endl;
+    std::cout << command << std::endl;
+  }
+  return command;
+}
+
+int32_t open_x_enviroment(XEnvironment *x_env, ProgramArgs args) {
+  x_env->display = XOpenDisplay("");
+  if (x_env->display == NULL) {
     std::cerr << "Failed to open display" << std::endl;
     return 1;
   } else if (args.verbose_mode) {
     std::cout << "Display opened successfully" << std::endl;
   }
 
-  Window root_window = XDefaultRootWindow(display);
+  x_env->root_window = XDefaultRootWindow(x_env->display);
 
   int32_t grab_status =
-      XGrabPointer(display, root_window, False, ButtonReleaseMask,
+      XGrabPointer(x_env->display, x_env->root_window, False, ButtonReleaseMask,
                    GrabModeAsync, GrabModeAsync, None, None, CurrentTime);
   if (grab_status != GrabSuccess) {
     std::cerr << "Something went wrong while grabbing the pointer" << std::endl;
+    return 1;
+  }
+  return 0;
+}
+
+int main(int argc, char **argv) {
+  ProgramArgs args;
+  int32_t valid_args = load_args(argc, argv, &args);
+  if (valid_args != 0)
+    return 1;
+
+  XEnvironment x_env;
+  int32_t x_env_status = open_x_enviroment(&x_env, args);
+  if (x_env_status != 0) {
     return 1;
   }
 
@@ -80,9 +117,9 @@ int main(int argc, char **argv) {
   memset(&scrot_rectangle, 0, sizeof(ScrotRectangle));
 
   while (select_mode_active) {
-    XSelectInput(display, root_window, ButtonReleaseMask);
+    XSelectInput(x_env.display, x_env.root_window, ButtonReleaseMask);
     XEvent event;
-    XNextEvent(display, &event);
+    XNextEvent(x_env.display, &event);
 
     if (event.type == ButtonRelease) {
       switch (event.xbutton.button) {
@@ -105,30 +142,14 @@ int main(int argc, char **argv) {
         break;
       }
     }
+
+    XClearArea(x_env.display, x_env.root_window, 100, 100, 1000, 1000, false);
   }
 
-  std::string file_name_template = "%Y-%m-%d-%T-screenshot.png";
-  std::string options =
-      "-a " + std::to_string(scrot_rectangle.x1) + "," +
-      std::to_string(scrot_rectangle.y1) + "," +
-      std::to_string(scrot_rectangle.x2 - scrot_rectangle.x1) + "," +
-      std::to_string(scrot_rectangle.y2 - scrot_rectangle.y1) + " ";
-
-  std::string command =
-      "scrot " + options + " " + args.path_to_screenshots + file_name_template;
-
-  if (args.verbose_mode) {
-    std::cout << "Scrot Rectangle: " << std::endl;
-    std::cout << "[ " << scrot_rectangle.x1 << ", " << scrot_rectangle.y1
-              << " ]" << std::endl;
-    std::cout << "[ " << scrot_rectangle.x2 - scrot_rectangle.x1 << ", "
-              << scrot_rectangle.y2 - scrot_rectangle.y1 << " ]" << std::endl;
-    std::cout << command << std::endl;
-  }
-
+  std::string command = build_scrot_command(args, scrot_rectangle);
   system(command.c_str());
 
-  XUngrabPointer(display, CurrentTime);
-  XCloseDisplay(display);
+  XUngrabPointer(x_env.display, CurrentTime);
+  XCloseDisplay(x_env.display);
   return 0;
 }
